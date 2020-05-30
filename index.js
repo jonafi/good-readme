19e6a3b104c4d5fd917e901cd089a5a97eb2c9d1
unknown <jonafi@gmail.com>
2020-05-30 15:25:52 -0500

    complete index.js rewrite. made async/await. added profile picture and email questions originally missed

--- index.js
+++ index.js
@@ -1,143 +1,151 @@
-let inquirer = require("inquirer");
-let fs = require('fs');
+const axios = require("axios");
+const inquirer = require("inquirer");
+const fs = require('fs');
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "userName",
-    message: "What is your github username?"
-  },
+makeReadMe();
 
-  {
-    type: "input",
-    name: "title",
-    message: "What is the project title?"
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please describe the project:"
-  },
-  {
-      type: "list",
-      message: "License Type:",
-      name: "license",
-      choices: [
-        "MIT", 
-        "GNU", 
-        "ISC", 
-        "Apache"
-      ]
-    },
-  {
-    type: "input",
-    name: "installation",
-    message: "Installation Instructions:"
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Installation Instructions:"
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Usage Instructions:"
-  },
-  {
-  type: "input",
-  name: "contributors",
-  message: "Additional Authors:"
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Tests:"
-  },
-  {
-    type: "input",
-    name: "questions",
-    message: "Questions:"
-    },
-  // {
-  //   type: "checkbox",
-  //   message: "What languages do you know?",
-  //   name: "stack",
-  //   choices: [
-  //     "HTML", 
-  //     "CSS", 
-  //     "JavaScript", 
-  //     "MySQL"
-  //   ]
-  // },
-]).then(function(data) {
+async function makeReadMe() {
+  try {
+    const  user  = await inquirer.prompt([
+      {
+        type: "input",
+        name: "userName",
+        message: "What is your github username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Email?"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the project title?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please describe the project:"
+      },
+      {
+          type: "list",
+          message: "License Type:",
+          name: "license",
+          choices: [
+            "MIT", 
+            "GNU", 
+            "ISC", 
+            "Apache"
+          ]
+        },
+      {
+        type: "input",
+        name: "installation",
+        message: "Installation Instructions:"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Installation Instructions:"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Usage Instructions:"
+      },
+      {
+      type: "input",
+      name: "contributors",
+      message: "Additional Authors:"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Tests:"
+      },
+    ]);
 
-  let filename = "README.md";
-  let content = `
-  # ${data.title}
+    const { data } = await axios.get(      
+      `https://api.github.com/users/${user.userName}`
+    );
+   //console.log(data.avatar_url)
+      let profilePic = data.avatar_url;
+      let filename = "README.md";
+      //to do fix weird tabbing issue;
+      let content = `
+# ${user.title}
 
-  ## Description 
+## Description 
 
-  ${data.description}
-
-
-  ## Table of Contents
-
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  * [Additional-Badges](#additional-badges)
+${user.description}
 
 
-  ## Installation
+## Table of Contents
 
-  ${data.installation}
-
-
-  ## Usage 
-
-  ${data.usage}
-
-  ## License
-
-  ![alt text](https://img.shields.io/github/license/${data.userName}/${data.title}.svg "License")
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Additional-Badges](#additional-badges)
 
 
-  ## Contributing
+## Installation
 
-  ${data.contributors}
-
-  ## Tests
-
-  ${data.tests}
-
-  ## Questions
-
-  ${data.questions}
-
-  ## Additional-Badges
-
-  ![alt text](https://img.shields.io/github/languages/top/${data.userName}/${data.title} "Top Language Used")
+${user.installation}
 
 
+## Usage 
 
-  ---
-  © 2020 Jonafi
-  `
-  ;
+${user.usage}
+
+## License
+
+![alt text](https://img.shields.io/github/license/${user.userName}/${user.title}.svg "License")
+
+
+## Contributing
+
+${user.contributors}
+
+## Tests
+
+${user.tests}
+
+## Questions
+
+Please direct any questions to ${user.email}
+
+Here is my profile picture for some reason:
+
+![Profile Picture](${profilePic})
+
+
+## Additional-Badges
+
+![alt text](https://img.shields.io/github/languages/top/${user.userName}/${user.title} "Top Language Used")
 
 
 
+---
+© 2020 Jonafi
+      `
+      ;
+      fs.writeFile(filename, content, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+    
+      });
+
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 
-  fs.writeFile(filename, content, function(err) {
 
-    if (err) {
-      return console.log(err);
-    }
 
-  });
-});
+
